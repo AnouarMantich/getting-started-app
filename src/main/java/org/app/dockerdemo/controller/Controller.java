9b6a8242f218ee2809bb632f50233548b1e4e4ab
AnouarMantich <a_mantich@etu.enset-media.ac.ma>
2025-11-13 12:42:08 +0100

    first commit

---
+++ src/main/java/org/app/dockerdemo/controller/Controller.java
@@ -0,0 +1,16 @@
+package org.app.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
